98f9f3a9573b7943c778d3639dba852b25699c1b
Whitewolf04 <36576380+Whitewolf04@users.noreply.github.com>
2020-01-29 21:57:01 -0500

    Finished Assignment 1

--- src/Assignment1/Appliance.java
+++ src/Assignment1/Appliance.java
@@ -1,5 +1,10 @@
 package Assignment1;
 
+/**
+ * Represents an appliance in an appliance store
+ * @author Tuan To
+ * @version 1.0.0
+ */
 public class Appliance {
 	private String type, brand;
 	private long serialNum;
@@ -7,7 +12,8 @@ public class Appliance {
 	public static int count = 0;
 	
 	/**
-	 * Default constructor
+	 * Create an empty appliance
+	 * Sets all attributes to default
 	 */
 	public Appliance() {
 		this.type = null;
@@ -17,6 +23,10 @@ public class Appliance {
 		count += 1;
 	}
 	
+	/**
+	 * Creates an appliance duplicating another appliance
+	 * @param Another appliance
+	 */
 	public Appliance(Appliance otherAppliance) {
 		otherAppliance.brand = this.brand;
 		otherAppliance.type = this.type;
@@ -26,53 +36,93 @@ public class Appliance {
 	}
 	
 	/**
-	 * Accessor and Mutator Methods
+	 * Gets the type of the appliance
+	 * @return Type of appliance as a string
 	 */
 	public String getType() {
 		return this.type;
 	}
+	
+	/**
+	 * Gets the brand of the appliance
+	 * @return Brand of appliance as a string
+	 */
 	public String getBrand() {
 		return this.brand;
 	}
+	
+	/**
+	 * Gets the appliance's serial number
+	 * @return A serial number which contains 7 digits
+	 */
 	public long getSerialNum() {
-		return this.getSerialNum();
+		return this.serialNum;
 	}
+	
+	/**
+	 * Gets the appliance's price
+	 * @return Price of the appliance in decimals
+	 */
 	public double getPrice() {
 		return this.price;
 	}
 	
+	/**
+	 * Sets the appliance's type
+	 * @param A type of appliance in string
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
+	
+	/**
+	 * Sets the appliance's brand
+	 * @param A brand of appliance in string
+	 */
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+	
+	/**
+	 * Sets the appliance's serial number
+	 * @param A 7-digit number
+	 */
 	public void setSerialNum(long serialNum) {
 		this.serialNum = serialNum;
 	}
+	
+	/**
+	 * Sets the appliance's price
+	 * @param Any real number larger than 1
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
 	
 	/**
-	 * toString method to be printed
+	 * Prints out the attributes of an appliance
+	 * @return A string displaying all attributes of the appliance
 	 */
 	public String toString() {
-		return("This product is a " + getType() + " from " + getBrand() + " with serial number " 
-				+ getSerialNum() + " has a price of " + getPrice());
+		return("Appliance Serial # " + getSerialNum()
+				+ "\nBrand: " + getBrand()
+				+ "\nType: " + getType()
+				+ "\nPrice: " + getPrice());
 	}
 	
 	/**
-	 * Finding the number of created appliance
-	 * The method uses a static variable to count all subclasses activities
-	 * @return the number of created appliance so far
+	 * Finds the number of appliance created
+	 * Static method to be called by the name of the class and be able to access all appliances' data
+	 * @return The number of created appliance so far
 	 */
 	public static int findNumberOfCreatedAppliance() {
 		return count;
 	}
 	
 	/**
-	 * Equals method to compare to appliance object
+	 * Compare two appliances to each other
+	 * @param Another appliance
+	 * @return Whether they are the same or not
 	 */
 	public boolean equals(Object otherObject) {
 		if(otherObject == null) {
@@ -86,6 +136,37 @@ public class Appliance {
 		}
 	}
 	
+	/**
+	 * Finds appliances by their brand
+	 * Static method to access all appliances data
+	 * @param A brand the user is looking for
+	 * @param An array of appliances to look into
+	 */
+	public static void findApplianceByBrand(String brandLF, Appliance[] inventory) {
+		for(int i = 0; i < inventory.length; i++) {
+			if(inventory[i] != null) { // Check if there is an appliance in the current spot
+				if(inventory[i].getBrand().equalsIgnoreCase(brandLF)) {
+					System.out.println(inventory[i] + "\n");
+					System.out.println("---------------------------------------\n");
+				}
+			}
+		}
+	}
 	
+	/**
+	 * Finds appliances under a certain price
+	 * @param The maximum price that the user wants
+	 * @param An array of appliances to look into
+	 */
+	public static void findCheaperThan(double priceLF, Appliance[] inventory) {
+		for(int i = 0; i < inventory.length; i++) {
+			if(inventory[i] != null) {
+				if(inventory[i].getPrice() < priceLF) {
+					System.out.println(inventory[i] + "\n");
+					System.out.println("--------------------------------------\n");
+				}
+			}
+		}
+	}
 	
 }
--- src/Assignment1/Driver.java
+++ src/Assignment1/Driver.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// Welcome Banner
 		System.out.println("\t*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*");
 		System.out.println("\t*                                               *");
@@ -19,7 +18,7 @@ public class Driver {
 		int maxAppliances, choice, countPass = 0;
 		String passEntered;
 		boolean exit = false;
-		final String PASSWORD = "c249";
+		final String PASSWORD = "c249"; // Set password as unchangeable
 		
 		
 		// 2. User-prompt:
@@ -37,80 +36,96 @@ public class Driver {
 			System.out.println("\t4.\tDisplay all appliances under a certain price");
 			System.out.println("\t5.\tQuit");
 			System.out.print("Please enter your choice > ");
-
-			// Checking if the choice is valid
-			int[] choiceArray = {1, 2, 3, 4, 5};
-			boolean rightChoice = false;
-			do {
-				choice = keyIn.nextInt();
-				for(int i = 0; i < 5; i++) {
-					if(choice == choiceArray[i]) {
-						rightChoice = true;
-					} else {
-						rightChoice = false;
-					}
-				}
-			}while (!rightChoice);
+			choice = keyIn.nextInt();
+			System.out.println();
 
 			// 3. Core logic:
 			switch(choice) {
 				case 1:
-					int countPassInner = 0; // To count the number of times the password has been entered wrong
+					int countPassInner = 0; // To count the number of times the password has been entered wrong inside choice 1
 					boolean pass = false; // To exit the do-while-loop if needed
-					System.out.print("Please enter the password > ");
 					do {
+						System.out.print("Please enter the password > ");
 						passEntered = keyIn.next();
-						if(passEntered.equals(PASSWORD)) {
+						if(passEntered.equals(PASSWORD)) {// Check if password is correct
+							// If password entered correctly, reset all attempts
+							countPassInner = 0;
+							countPass = 0;
+							
+							// Check if inventory is full
 							if(Appliance.findNumberOfCreatedAppliance() < maxAppliances) {
+								// Create a new appliance
 								Appliance newApp = new Appliance();
 								System.out.print("Type of appliance > ");
-								newApp.setType(keyIn.next());
+								String typeEntered = keyIn.next();
+								newApp.setType(typeEntered);
 								System.out.print("Brand of appliance > ");
-								newApp.setBrand(keyIn.next());
+								String brandEntered = keyIn.next();
+								newApp.setBrand(brandEntered);
 								System.out.print("Serial Number > ");
-								newApp.setSerialNum(keyIn.nextLong());
+								long serialEntered = keyIn.nextLong();
+								newApp.setSerialNum(serialEntered);
 								System.out.print("Price > ");
-								newApp.setPrice(keyIn.nextDouble());
+								double priceEntered = keyIn.nextDouble();
+								newApp.setPrice(priceEntered);
+								System.out.println();
 								
-								inventory[Appliance.findNumberOfCreatedAppliance()] = newApp;
+								boolean exist = false;	// To check if the appliance already exist
+								for(int i = 0; i < inventory.length; i++) {
+									if(inventory[i] != null && inventory[i].equals(newApp)) {
+										exist = true;
+									}
+								}
+								
+								// If the appliance hasn't existed, add it to inventory
+								if(exist == false) {
+									inventory[Appliance.findNumberOfCreatedAppliance()-1] = newApp;
+								} else {
+									System.out.println("Appliance already exist!");
+								}
 							} else {
-								System.out.println("Inventory is full!");
+								System.out.println("Inventory is full!\n");
 							}
-							pass = true;
+							pass = true;	// Exit this do-while loop for choice 1
 						} else {
-							if(countPassInner < 3) {
+							if(countPassInner < 2) {
+								System.out.println("Password incorrect! Please try again!\n");
 								countPassInner += 1;
-							} else if (countPass < 12) {
+								continue;
+							} else if (countPass < 9) {
+								System.out.println("Password was entered incorrectly 3 times!!!\n");
+								countPassInner += 1;
 								countPass += countPassInner;
-								pass = true;
-							} else if(countPass == 12) {// When password was entered incorrectly 12 times, the program exit
+								pass = true; // Exit this do-while loop to go back to the main menu
+							} else if(countPass == 9) {// The count set to 9 for the last 3 attempts are the final attempts
+								// When password was entered incorrectly 12 times, the program exit
 								System.out.println("Program detected suspicious activities and will determinate immediately!");
 								pass = true; // To exit this do-while loop
 								exit = true; // To exit the entire program
 							}
 						}
 					} while(!pass);
-					break;
+					break;	// Prevent the program from going into 2nd choice
 				case 2:
 					// Reset the tracking variables
 					countPassInner = 0;
 					pass = false;
 					
-					System.out.print("Please enter the password > ");
 					do {
+						System.out.print("Please enter the password > ");
 						long seriEntered;
-						int index = -1;
+						int index = -1;		// Index for the specific item chosen to be changed
+						boolean passOpt2 = false;	// Modifier for inner do-while loop of choice 2
 						passEntered = keyIn.next();
 						if(passEntered.equals(PASSWORD)) {
+							countPassInner = 0;
+							countPass = 0;
 							System.out.print("Please enter the serial number > ");
 							seriEntered = keyIn.nextLong();
 							for(int i = 0; i < inventory.length; i++) {
 								if(seriEntered == inventory[i].getSerialNum()) {
-									index = i;
-									System.out.println("Appliance Serial # " + inventory[i].getSerialNum());
-									System.out.println("Brand: " + inventory[i].getBrand());
-									System.out.println("Type: " + inventory[i].getType());
-									System.out.println("Price: " + inventory[i].getPrice());
+									index = i;	// Mark the index for future changes
+									System.out.println(inventory[i] + "\n");
 								}
 							}
 							
@@ -120,34 +135,141 @@ public class Driver {
 								System.out.println("Do you wish to: ");
 								System.out.println("\t1.\tTry again");
 								System.out.println("\t2.\tGo back to the main menu");
+								System.out.println("\t(Enter number to go back to the main menu)");
 								System.out.print("Please enter your choice > ");
-								//************************Need error handling
-								switch(keyIn.nextInt()) {
+								
+								int choiceEntered = keyIn.nextInt();
+								switch(choiceEntered) {
 								case 1:
+									System.out.println();
 									continue;
-								case 2:
-									break;
+								default:
+									System.out.println();
 								}
 								break;
 							}
 							
-							// Prompt the user which information to change
-							System.out.println("What information would you like to change?");
-							System.out.println("\t1.\tbrand");
-							System.out.println("\t2.\ttype");
-							System.out.println("\t3.\tprice");
-							System.out.println("\t4.\tQuit");
-							System.out.print("Please enter your choice > ");
-							//**********************************Need error handling
-							
-							switch(keyIn.nextInt()) {
-							case 1:
-								
+							do {
+								// Prompt the user which information to change
+								System.out.println("What information would you like to change?");
+								System.out.println("\t1.\tbrand");
+								System.out.println("\t2.\ttype");
+								System.out.println("\t3.\tprice");
+								System.out.println("\t4.\tQuit");
+								System.out.print("Please enter your choice > ");
+								int changeChoice = keyIn.nextInt();
+
+								switch(changeChoice) {
+								case 1:
+									// Ask for input of new brand and print out appliance after modify
+									System.out.print("Please enter the new brand > ");
+									String brandEntered = keyIn.next();
+									inventory[index].setBrand(brandEntered);
+									System.out.println("\n" + inventory[index]);
+									break;
+								case 2:
+									// Ask for the input of new type and print out appliance after modify
+									System.out.print("Please enter the new type > ");
+									String typeEntered = keyIn.next();
+									inventory[index].setType(typeEntered);
+									System.out.println("\n" + inventory[index]);
+									break;
+								case 3:
+									// Ask for the input of new price and print out appliance after modify
+									System.out.print("Please enter the new Price > ");
+									double priceEntered = keyIn.nextDouble();
+									inventory[index].setPrice(priceEntered);
+									System.out.println("\n" + inventory[index]);
+									break;
+								case 4:
+									// Only quit the loop when option 4 is chosen
+									passOpt2 = true;
+									System.out.println();
+									break;
+								default:
+									System.out.println("Choice invalid! Please try again!\n");
+								}
+							} while(!passOpt2);
+							pass = true;
+						} else {
+							if(countPassInner < 2) {
+								System.out.println("Password incorrect! Please try again!\n");
+								countPassInner += 1;
+								continue;
+							} else if (countPass < 9) {
+								System.out.println("Password was entered incorrectly 3 times!!!\n");
+								countPassInner += 1;
+								countPass += countPassInner;
+								pass = true;
+							} else if(countPass == 9) {
+								// When password was entered incorrectly 12 times, the program exit
+								System.out.println("Program detected suspicious activities and will determinate immediately!");
+								pass = true; // To exit this do-while loop
+								exit = true; // To exit the entire program
 							}
 						}
 					} while(!pass);
+					break;
+				case 3:
+					// Ask for the brand looking for and print out all appliances of that brand
+					pass = false;
+					do {
+						System.out.print("Please enter the brand you are looking for > ");
+						String brandEntered = keyIn.next();
+						Appliance.findApplianceByBrand(brandEntered, inventory);
+						
+						System.out.println("Search done! What do you want to do?");
+						System.out.println("\t1.\tSearch again");
+						System.out.println("\t2.\tGo back to the main menu");
+						System.out.println("\t(Enter any number (except 1) to return to the main menu)");
+						System.out.print("Please enter your choice > ");
+						
+						int choiceEntered = keyIn.nextInt();
+						System.out.println();
+						switch(choiceEntered) {
+						case 1:
+							break;
+						default:
+							pass = true;
+							break;
+						}
+					} while(!pass);
+					break;
+				case 4:
+					pass = false;
+					do {
+						System.out.print("Please enter the maximum price > ");
+						double priceEntered = keyIn.nextDouble();
+						Appliance.findCheaperThan(priceEntered, inventory);
+						
+						System.out.println("Search done! What do you want to do?");
+						System.out.println("\t1.\tSearch again");
+						System.out.println("\t2.\tGo back to the main menu");
+						System.out.println("\t(Enter any number (except 1) to return to the main menu)");
+						System.out.print("Please enter your choice > ");
+						
+						int choiceEntered = keyIn.nextInt();
+						System.out.println();
+						switch(choiceEntered) {
+						case 1:
+							break;
+						default:
+							pass = true;
+							break;
+						}
+					} while(!pass);
+					break;
+				case 5:
+					exit = true;
+					System.out.println("\n\t\tThank you for coming to our store!!!");
+					break;
+				default:
+					System.out.println("Choice invalid! Please try again!\n");
 			}
 		} while (!exit);
+		
+		// 4. Clean-up:
+		keyIn.close();	// To make Java happy!
 	}
 
 }
